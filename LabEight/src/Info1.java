47563ca0c094dc295bf3c441c46da5f60f998aee
Rafid <rafid.ahmmad.3@gmail.com>
2020-11-29 00:07:18 +0600

    committing lab eight class works

---
+++ LabEight/src/Info1.java
@@ -0,0 +1,23 @@
+public class Info1 {
+    int numer;
+    int denom;
+
+    public Info1(int numer, int denom) {
+        this.numer = numer;
+        this.denom = denom;
+    }
+
+    public void displayRaw() {
+        System.out.println("constructor passed values " + numer + " and " + denom);
+    }
+
+    public void add(Info1 fraction) {
+        this.numer = fraction.numer;
+        this.denom = fraction.denom;
+        System.out.println(numer + "/" + denom);
+    }
+
+    public String toString() {
+        return "the passed parameters are " + numer + " and " + denom;
+    }
+}
