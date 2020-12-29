0c4d18e4b75c281904dbd40961b1e6734fcd8d29
Rafid <rafid.ahmmad.3@gmail.com>
2020-12-30 00:45:36 +0600

    committing lab 12 class task file io

---
+++ LabTwelveFileIOClassTasks/src/FileAppendOperation.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileAppendOperation {public static void main(String[] args) {
+    PrintWriter output = null;
+    String filename = "test.txt";
+    try {
+        output = new PrintWriter(new FileOutputStream(filename, true));
+    } catch (FileNotFoundException e) {
+        System.out.println("file not found exception caught");
+    }
+
+    System.out.println("enter few strings");
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < 3; i++) {
+        String ln = sc.nextLine();
+        output.println(i+ " th input "+ ln);
+    }
+    output.close();
+    System.out.println("lines written successful");
+}
+}
---
+++ LabTwelveFileIOClassTasks/src/FileReadClass.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReadClass {
+    public static void main(String[] args) {
+        String filename = "test.txt";
+        Scanner input = null;
+        System.out.println("there file contains the following strings");
+
+        try {
+            input = new Scanner(new File(filename));
+
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+        }
+        while (input.hasNextLine()){
+            String line = input.nextLine();
+            System.out.println(line);
+        }
+    }
+}
