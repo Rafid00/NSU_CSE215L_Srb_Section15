37b3c8d9abfd3f8f498441a7d03624eaacd3d90b
Rafid <rafid.ahmmad.3@gmail.com>
2021-01-23 22:29:00 +0600

    submitting Lab 10 class task

---
+++ labTenClassTask/src/com/rafid/Main.java
@@ -0,0 +1,22 @@
+package com.rafid;
+
+public class Main {
+
+
+    public static void main(String args[]) {
+
+        Circle circ = new Circle("Black", false, 5.25);
+
+        System.out.println(circ.getArea());
+
+        System.out.println(circ.toString());
+
+        circ.setColor("purple");
+
+        System.out.println(circ.getColor());
+
+        GeometricObject geo = new GeometricObject();
+
+        System.out.println(geo.toString());
+    }
+}
